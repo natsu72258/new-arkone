c4abd94e7f0bd77b205076c6905930c827becab2
natsu72258 <64302361+natsu72258@users.noreply.github.com>
2020-08-15 15:55:52 +0800

    fb icon share

--- js/main.js
+++ js/main.js
@@ -8,4 +8,7 @@ $(function(){
 			$("header > nav").addClass("open")
 		}	
 	})
+	$("#FB").on("click",function(){
+            window.open('http://www.facebook.com/share.php?u='.concat(encodeURIComponent(location.href)),"_blank","toolbar=yes,location=yes,directories=no,status=no,menubar=yes,scrollbars=yes,resizable=no,copyhistory=yes,width=600,height=400" )
+        })
 })
