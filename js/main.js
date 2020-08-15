2e2fa558fff121fca6716429c35dc05249de50c2
natsu72258 <64302361+natsu72258@users.noreply.github.com>
2020-08-15 14:32:03 +0800

    畢專

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+	$("#menubtn").on("click", function(){
+		if ($("header > nav").hasClass("open")) {
+			$("header > nav").removeClass("open")
+		}else{
+			$("header > nav").addClass("open")
+		}	
+	})
+})
