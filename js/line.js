f6f15b7bc6c165b2dda305bb62d2673f5ef5a28b
natsu72258 <64302361+natsu72258@users.noreply.github.com>
2020-12-21 14:02:30 +0800

    latest2
    000000000000

--- js/line.js
+++ js/line.js
@@ -7,9 +7,9 @@ window.onload = function(){
     }
     
     document.getElementById("LinePc").onclick = function(){
-        window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://mikecheng1208.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+        window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://natsu72258.github.io/new-arkone/index.html"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
     document.getElementById("Linemobile").onclick = function(){
-        window.open('line://msg/text/'+ encodeURIComponent("https://mikecheng1208.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+        window.open('line://msg/text/'+ encodeURIComponent("https://natsu72258.github.io/new-arkone/index.html"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
 }
